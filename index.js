a10a9eb5cd37c45b417628917a4a34200ca57dd1
nicolashedoire <nicolashedoire@hotmail.com>
2020-08-25 16:32:00 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const pretty = require('express-prettify');
+const cors = require('cors');
+const helmet = require("helmet");
+
+const app = express();
+
+app.use(helmet());
+app.use(pretty({ query: 'pretty' }));
+app.use(cors());
+
+app.get('/', function (req, res) {
+  res.send('Welcome in Movies API');
+});
+
+app.listen(5000, () => {
+  console.log('API Movies listen on port 5000');
+})
