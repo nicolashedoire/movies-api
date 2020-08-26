f7219ed07785ec1a098877c30c0301384edf3cd5
nicolashedoire <nicolashedoire@hotmail.com>
2020-08-26 09:18:23 +0200

    remove useless config

--- db.js
+++ db.js
@@ -3,15 +3,15 @@ const { Pool } = require('pg');
 
 dotenv.config();
 
-console.log(dotenv.config());
+console.log(process.env);
 
-const dbDevConfig = {
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  database: process.env.DB_NAME,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-};
+// const dbDevConfig = {
+//   host: process.env.DB_HOST,
+//   port: process.env.DB_PORT,
+//   database: process.env.DB_NAME,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+// };
 
 const dbProdConfig = {
   connectionString: process.env.DATABASE_URL,
@@ -24,7 +24,7 @@ pool.connect((err, client) => {
     if (err) {
       console.log(err);
     }
-    console.log(`connection à la base de données ${client.database} ok...`);
+    console.log(`connection à la base de données ok...`);
 });
 
 const execQuery = async (query, values) => {
